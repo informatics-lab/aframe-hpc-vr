88f991251e8f4f824f05cad70e70b5bbdab1ebe5
Thomas Powell <t.powell.meto@gmail.com>
2017-12-07 15:58:28 +0000

    add images and latest code

--- src/js/HudText.js
+++ src/js/HudText.js
@@ -3,13 +3,16 @@
 module.exports = AFRAME.registerComponent('hud-text', {
     schema: {
         triggerEvents: {type: 'array'},
-        text: {type: 'string', default: ''},
+        hideEvents:{type:'array'},
+        eventTextKey: {type:'string', default:'text'},
         globalListener: {type: 'boolean', default: false},
     },
 
     init: function () {
         const self = this;
-        self.playSrcBound = self.playSrc.bind(self);
+
+        self.typeTextBound = self.typeText.bind(self);
+        self.hideTextBound = self.hideText.bind(self);
     },
 
     update: function () {
@@ -17,28 +20,62 @@ module.exports = AFRAME.registerComponent('hud-text', {
 
         self.listener = (self.data.globalListener) ? self.el.sceneEl : self.el;
 
+        let typingContainer = document.createElement('div');
+        typingContainer.setAttribute('id','typingContainer');
+        document.body.appendChild(typingContainer);
+
+        let typingText = document.createElement('div');
+        typingText.setAttribute('class','text');
+        typingContainer.appendChild(typingText);
+
+        let typingBg = document.createElement('div');
+        typingBg.setAttribute('class','bg');
+        typingContainer.appendChild(typingBg);
+
+        self.typingContainer = typingContainer;
+
         self.removeEventListeners();
         self.addEventListeners();
+
+
     },
 
-    playSrc: function (event) {
+    typeText: function (event) {
         const self = this;
-        console.log("triggering");
-        self.data.src.playbackRate = 4.0;
-        self.data.src.play();
+        self.typingContainer.setAttribute("style", "display:block;");
+
+        let typed = new Typed('#typingContainer .text', {
+            strings: ['^1000 Welcome to the Met Office HPC VR Tour <br/><span class=\"smaller\">open the menu to begin...</span>'],
+            typeSpeed: 30,
+            showCursor:false
+        });
+
     },
+
+    hideText: function(event) {
+        const self = this;
+        self.typingContainer.setAttribute("style", "display:none;");
+    } ,
 
     addEventListeners: function () {
         const self = this;
         self.data.triggerEvents.map(function (eventName) {
-            self.listener.addEventListener(eventName, self.playSrcBound);
+            self.listener.addEventListener(eventName, self.typeTextBound);
+        });
+
+        self.data.hideEvents.map(function(eventName) {
+           self.listener.addEventListener(eventName, self.hideTextBound)
         });
     },
 
     removeEventListeners: function () {
         const self = this;
         self.data.triggerEvents.map(function (eventName) {
-            self.listener.removeEventListener(eventName, self.playSrcBound);
+            self.listener.removeEventListener(eventName, self.typeTextBound);
+        });
+
+        self.data.hideEvents.map(function(eventName) {
+            self.listener.removeEventListener(eventName, self.hideTextBound);
         });
     }
 

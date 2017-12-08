aa468f7ed58544e104c4cd823a9b21da0307f76d
Megan Fitzsimons <megan.fitzsimons@informaticslab.co.uk>
2017-12-08 15:36:33 +0000

    captioned photos

--- src/js/HudText.js
+++ src/js/HudText.js
@@ -52,22 +52,22 @@ module.exports = AFRAME.registerComponent('hud-text', {
         if(event) {
             switch(event.detail.src) {
                 case '#hq-outside':
-                    self.text = '^200 You are now outside Met Office HQ, Exeter<br/><span class="smaller">open the menu to continue...</span>';
+                    self.text = '^200 You are now outside Met Office HQ, Exeter. Our Headquarters are home to two IT Halls, the energy needed to power these is equivalent to energy needed to power the whole of Exeter.<br/><span class="smaller">open the menu to continue...</span>';
                     break;
                 case '#hq-hpc':
-                    self.text = '^200 some text about hpc in it hall 1<br/><span class="smaller">open the menu to continue...</span>';
+                    self.text = '^200 This is the Cray XC40 Supercomputer. Each of these machines were installed in 3 phases and are capable of perfoming 14 petaflops of operations per second.<br/><span class="smaller">open the menu to continue...</span>';
                     break;
                 case '#hq-servers':
-                    self.text = '^200 some text about servers in it hall 1<br/><span class="smaller">open the menu to continue...</span>';
+                    self.text = '^200 The Met Office has 300 servers running Linux at any given time. <br/><span class="smaller">open the menu to continue...</span>';
                     break;
                 case '#hq-storage':
-                    self.text = '^200 some text about storage in it hall 1<br/><span class="smaller">open the menu to continue...</span>';
+                    self.text = '^200 This is the HPC data storage, where we store the results of our weather and climate research. Each unit is capable of storing up to 400 petabytes of data. 1<br/><span class="smaller">open the menu to continue...</span>';
                     break;
                 case '#cb-outside':
-                    self.text = '^200 some text about collaboration building<br/><span class="smaller">open the menu to continue...</span>';
+                    self.text = '^200 You’re now standing outside of our collaboration building, located at Exeter Science Park. This building is home to the latest HPC addition and various Met Office teams.<br/><span class="smaller">open the menu to continue...</span>';
                     break;
                 case '#cb-hpc':
-                    self.text = '^200 some text about hpc at the collaboration buildng<br/><span class="smaller">open the menu to continue...</span>';
+                    self.text = '^200 The collaboration building HPC is used primarily for research and collaboration data and <br/><span class="smaller">open the menu to continue...</span>';
                     break;
                 case '#cb-servers':
                     self.text = '^200 some text about servers at the collaboration building<br/><span class="smaller">open the menu to continue...</span>';

88b16439c25858492b05fd47a01d96ce35262231
Thomas Powell <t.powell.meto@gmail.com>
2017-12-12 15:05:45 +0000

    alter text

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,11 @@
 'use strict';
 
 // core library
-// import 'aframe';
+import 'aframe';
 
 // Component that sets a property on a given event
 // see: https://github.com/ngokevin/kframe/tree/master/components/event-set/
 import 'aframe-event-set-component';
-
-// Shader for using .gifs as textures
-// see: https://github.com/mayognaise/aframe-gif-shader
-import 'aframe-gif-shader';
 
 // Shader for allowing transparencies in video textures.
 // see: https://github.com/nikolaiwarner/aframe-chromakey-material
